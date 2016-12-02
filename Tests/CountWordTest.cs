b6f2181544a475cf26b5cd3a89d6b19d990cf257
James Howard <james@>
2016-12-02 13:45:27 -0800

    Add first test from spec. It will have Object populated

---
+++ Tests/CountWordTest.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using WordCounter.Objects;
+
+namespace CountWordTest
+{
+  public class RepeatCounterTest
+  {
+    [Fact]
+    public void CountWord_CountsWordRepetedInString_True()
+    {
+      int count = 1;
+      int result = RepeatCounter.CountRepeats("dog", "dog");
+      Assert.Equal(count, result);
+    }
+  }
+}
